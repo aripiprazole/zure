26cc3ea42e9277c811de117ebc26c1413fcfeac9
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-09-15 12:55:45 -0300

    test(leibnz): makes type high order

--- test_suite/leibnz.ml
+++ test_suite/leibnz.ml
@@ -6,30 +6,30 @@
    let f : eq 10 10 = Refl
    ```*)
 type {'t} ('a : 't) ('b : 't) eq =
-  | Refl : ('a, 'a) eq
+  | Refl : eq 'a 'a
 
 (* Identity proof *)
-let eq_identity : {'a} -> eq ('a, 'a) -> eq ('a, 'a) =
+let eq_identity : {'a} -> eq 'a 'a -> eq 'a 'a =
   fun {'a} e -> e
 
 (* Symmetry proof *)
-let eq_symmetry : {'a, 'b} -> eq ('a, 'b) -> eq ('b, 'a) =
+let eq_symmetry : {'a, 'b} -> eq 'a 'b -> eq 'b 'a =
   fun {'a, 'b} -> function
   | Refl -> Refl
 
 (* Transitivity proof *)
-let eq_transitivity : {'a, 'b, 'c} -> eq ('a, 'b) -> eq ('b, 'c) -> eq ('a, 'c) =
+let eq_transitivity : {'a, 'b, 'c} -> eq 'a 'b -> eq 'b 'c -> eq 'a 'c =
   fun {'a, 'b, 'c} e e' ->
     match e, e' with
     | Refl, Refl -> Refl
 
 (* Congruence proof *)
-let eq_congruence : {'a, 'b, 'f : {'dom, 'cod} -> 'dom -> 'cod} -> eq ('a, 'b) -> eq ('f 'a, 'f 'b) =
+let eq_congruence : {'a, 'b, 'f : {'dom, 'cod} -> 'dom -> 'cod} -> eq 'a 'b -> eq ('f 'a) ('f 'b) =
   fun {'a, 'b, 'f} -> function
   | Refl -> Refl
 
 (* Using syntax sugar of `:=` *)
-let (:=) e e' = eq (e, e')
+let (:=) e e' = eq e e'
 
 (* Identity proof *)
 let identity : {'a} -> ('a := 'a) -> ('a := 'a) =
